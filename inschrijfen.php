ea4f43177579c2b3adb48bce6c44afa8c8833917
grimvoid <kajhorseling@hotmail.nl>
2017-06-14 10:06:24 +0200

    front end update

--- inschrijfen.php
+++ inschrijfen.php
@@ -5,7 +5,7 @@
 		<link href="websiteStyle.css" rel="stylesheet" type="text/css" />
 </head>
 
-<Marquee>Ik behandel mij dieren als famillie</Marquee>
+<Marquee>hier kunt u inschrijfen</Marquee>
 
 <body>
 
@@ -22,65 +22,60 @@
 
 <hr> 
 
-<h2> Die van mij waren vroeger </h2>
-	 
-<li type="disc"> Cotton
-<li type="disc"> Nina
-<li type="disc"> Sneeuw
-
-<br>
- 
-<h1><p><font color="blue">cotton </font></p></h1>
-
-<h2><p><font color="blue">
-
-Cotton was mijn eerste vogel als huisdier. We kregen hem van mijn vader omdat mijn vaders vriend was gestorven.
-Hij bleef bij ons een jaartje. We hadden maar een jaartje met hem omdat zijn bloedvaten dichtwaren. We hadden dit niet opgemerkt.
-op een dag begon Cotton raar te schreeuwen. We gaven hem medicijn maar het lukte niet. Mijn broer bracht hem naar de vogel kliniek
-in IJburg. De Dierendokter deed er alles aan om hem te genezen maar het lukte niet. Hij is pijnlijk doodgegaan aan dit probleem.
-Ik heb geen foto gemaakt omdat dit te pijnlijk voor me was.
-
-</font></p></h2>
-
-<br>
-<img src="cotton.png" alt="cotton" width="300" height="240">
- 
-<br> 
-
-<h1><p><font color="green">Nina </font></p></h1>
-
-<h2><p><font color="green">
-
-Nina was mijn tweede vogel. We hadden haar gekocht omdat De eerste vogel Cotton zich alleen voelde.
-Ze was altijd lief tegen Cotton. Als hij jeuk had dat krabt ze hem achter zijn nek. Maar als het om eten gaat dan
-bijt ze altijd deze vogel. Nina wilt vaak niet delen met andere vogels. Daarom heb ik twee voerbakken geplaats ver van elkaar.
-Nina is niet kieskeurig over haar voedsel. Ze eet bijna van alles en nog wat. Toen ik 1 keer van school kwam zag ik haar aan de plant te kauwen.
-Ook bijt ze vaak aan de kooi. Ze wilt sterk zijn dus ze moet alle ijzer binnen krijgen. Alles was goed totdat Cotton stierf.
-Nina was erg verdrietig over dit. Dus kochten we voor haar een nieuwe mannelijke vogel Sneeuw. Alles was goed totdat deze vogel wegvloog.
-Nina kon het niet meer aan dus vloog ze ook weg. Ik hoop echt dat alles goed is met haar.
-
-</font></p></h2>
-
-<br>
-<img src="nina.jpg" alt="nina" width="300" height="240">
-
-
-<br>
-
-<h1><p><font color="red">sneeuw </font></p></h1>
-
-<h2><p><font color="red">
-
-Sneeuw was onze tweede mannelijke vogel. Hij was 4 maanden oud toen we hem kochten uit de Pets Place XL in IJburg.
-Hij was vol met kracht en passie. inplaats van vliegen rent deze vogel vaak buiten de kooi. Deze vogel was vaak niet voorzichtig.
-Hij sprong altijd hoog van de kooi. 1 keer bloedde zijn snavel. Hij heet sneeuw omdat hij wit als sneeuw heet.
-Hij was ook de pluisichte van alle vogels. Hij voelde net als een kussen als ik hem aanraakte. voordat ik naar indonesia ging
-Deed mijn moeder de kleren buiten. Uit het niets kwam een reiger op onze balkon.Sneeuw schrok hier erg van en vloog weg.
-we probeerden hem te vinden maar het lukte niet. We hoorden hem schreeuwen maar we zagen hem niet.
-de andere vogel Nina was erg verdrietig hierover. Toen ik in indonesia was vloog ze weg van huis. Ik zag haar nooit meer.
-We hadden toen de vogel Haru gekocht maar die was eenzaam 3 weken alleen bij een van mijn vaders vrienden.
-
-</font></p></h2>
+<form method="post" action="10078708@edu.regiocollege.nl">
+		<table>
+			<tr>
+					<td>	<strong>	Persoons gegevens:	</strong>	</td>
+			</tr>
+			
+			<tr>
+					<td>	Naam:	</td>
+					<td>	<input type="text" name="Naam">		</td>
+			</tr>
+			
+			<tr>
+					<td>	Adress:		</td>
+					<td>	<input type="text" name="Adress">	</td>
+			</tr>
+			
+			<tr>
+					<td>	Postcode:	</td>
+					<td>	<input type="text" name="Postcode" maxlength="6">	</td>
+			</tr>
+			
+			<tr>
+					<td>	Woonplaats:	</td>
+					<td>	<input type="text" name="Woonplaats">	</td>
+			</tr>		
+			
+			<tr>
+					<td>	Homepage:	</td>
+					<td>	<input type="url" name="Homepage">	</td>
+			</tr>
+			
+			<tr>
+					<td>	E-mailadres:	</td>
+					<td>	<input type="email" name="E-mailadres">	</td>
+			</tr>
+			
+			<tr>
+			        <td colspan = "2">	<hr>	</td>
+			</tr>
+			
+			<tr>
+					<td>	<strong>	<em>	Heeft u nog vragen?	</em>	</strong>	</td>
+			</tr>
+			
+			<tr>
+					<td>	<textarea name="opmerkingen" cols="40" rows="10"></textarea>	<INPUT type="Reset"> <INPUT type="Submit">	</td>
+			</tr>
+		</table>
+		
+		
+		<table>
+		
+		</table>
+	</form>
 
 <br>
 <img src="sneeuw.jpg" alt="sneeuw" width="230" height="240">
