ac096a4304d6ff842039a3210bf6282c3d85eb21
miepvis <jelmar.r@hotmail.com>
2017-06-14 19:11:09 +0200

    hier is het

--- activiteiten.php
+++ activiteiten.php
@@ -23,25 +23,68 @@
 
 <h2>
 hier kunt u de activiteiten zien
-
-
-<h2>
+</h2>
 
 <center><table cellpanding="15" border="4">
-<caption align="bottom"></caption>
-	
-<tr
-	<strong><th colspan="7">activiteiten</strong></th></strong>
-	<strong><th colspan="7">Naam        </strong></th></strong>
-	<strong><th colspan="7">locatie     </strong></th></strong>
-	<strong><th colspan="7">stadium     </strong></th></strong>
-	<strong><th colspan="7">einddatum   </strong></th></strong>
-</tr>
-
-<tr>
-<tr>
-
-</tr>
-
+    <caption align="bottom"></caption>
+<?php
+    //database inloggegevens 
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    //                                  dit moet je even zelf invullen
+    $dbname = "???";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    //selecteren en nakijken
+    $sql = "SELECT * FROM Activiteit";
+    $result = $conn->query($sql);
+?>
+    //dit is de table
+    <tr>
+        <strong><th colspan="7">activiteiten</th>
+        <th colspan="7">Naam        </th>
+        <th colspan="7">locatie     </th>
+        <th colspan="7">stadium     </th>
+        <th colspan="7">einddatum  </th></strong>
+    </tr>
+<?php
+    //data laten zien
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["ID"] . "<br>";
+    }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+?>
+    <tr>
+        <strong><th colspan="7">Training</th>
+        <th colspan="7">Naam        </th>
+        <th colspan="7">locatie     </th>
+        <th colspan="7">datum     </th></strong>
+    </tr>
+<?php
+    //selecteren en nakijken
+    $sql = "SELECT * FROM Training";
+    $result = $conn->query($sql);
+    //data laten zien
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["ID"] . "<br>";
+    }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+?>
+</table></center>
 </body>
 </html>
--- inschrijfen.php
+++ inschrijfen.php
@@ -4,83 +4,86 @@
 		<title>ZaanRun</title>
 		<link href="websiteStyle.css" rel="stylesheet" type="text/css" />
 </head>
-
 <Marquee>hier kunt u inschrijfen</Marquee>
-
 <body>
-
 <a href="thuispagina.php">thuispagina</a>
 <a href="sponsor.php">sponsor</a>
 <a href="activiteiten.php">activiteiten</a>
 <a href="inschrijfen.php">inschrijfen</a>
 <a href="Contact.php">Contact</a>
-
-
 <hr>
-
 <h1> Inschrijfen </h1>
-
 <hr> 
-
 <form method="post" action="10078708@edu.regiocollege.nl">
 		<table>
 			<tr>
 					<td>	<strong>	Persoons gegevens:	</strong>	</td>
 			</tr>
-			
 			<tr>
 					<td>	Naam:	</td>
-					<td>	<input type="text" name="Naam">		</td>
+					<td>	<input type="text" name="Naam" required>		</td>
 			</tr>
-			
+            <tr>
+					<td>	Tussenvoegsel:	</td>
+					<td>	<input type="text" name="Tussenvoegsel" required>		</td>
+			</tr>
+            <tr>
+					<td>	Achteraam:	</td>
+					<td>	<input type="text" name="Achteraam" required>		</td>
+			</tr>
 			<tr>
 					<td>	Adress:		</td>
-					<td>	<input type="text" name="Adress">	</td>
+					<td>	<input type="text" name="Adress" required>	</td>
 			</tr>
-			
 			<tr>
-					<td>	Postcode:	</td>
-					<td>	<input type="text" name="Postcode" maxlength="6">	</td>
+					<td>	Geboortedatum:	</td>
+					<td>	<input type="text" name="Woonplaats" required>	</td>
 			</tr>
-			
-			<tr>
-					<td>	Woonplaats:	</td>
-					<td>	<input type="text" name="Woonplaats">	</td>
-			</tr>		
-			
-			<tr>
-					<td>	Homepage:	</td>
-					<td>	<input type="url" name="Homepage">	</td>
-			</tr>
-			
-			<tr>
+           //deze moet je nog even toevoegen aan je database
+            <tr>
 					<td>	E-mailadres:	</td>
-					<td>	<input type="email" name="E-mailadres">	</td>
+					<td>	<input type="email" name="E-mailadres" required>	</td>
 			</tr>
-			
+            <tr>
+					<td>	Groep:	</td>
+					<td>	<input type="text" name="Groep">	</td>
+			</tr>
 			<tr>
 			        <td colspan = "2">	<hr>	</td>
 			</tr>
-			
 			<tr>
 					<td>	<strong>	<em>	Heeft u nog vragen?	</em>	</strong>	</td>
 			</tr>
-			
 			<tr>
-					<td>	<textarea name="opmerkingen" cols="40" rows="10"></textarea>	<INPUT type="Reset"> <INPUT type="Submit">	</td>
+                <td>	<textarea name="opmerkingen" cols="40" rows="10"></textarea>	<INPUT type="Reset"></INPUT> <INPUT type="Submit"></INPUT></td>
 			</tr>
 		</table>
-		
-		
-		<table>
-		
-		</table>
-	</form>
-
+</form>
+<?php
+    //formulier gegevens
+    $Naam = $_POST["Naam"];
+    $Tussenvoegsel = $_POST["Tussenvoegsel"];
+    $Achteraam = $_POST["Achteraam"];
+    $Adress = $_POST["Adress"];
+    $Geboortedatum = $_POST["Geboortedatum"];
+    $Mail = $_POST["E-mailadres"];
+    $Groep = $_POST["Groep"];
+    //database inloggegevens 
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    //                                  dit moet je even zelf invullen
+    $dbname = "???";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    $sql = "INSERT INTO Loper
+    VALUES ($Naam.$Achteraam.$Tussenvoegsel.$Adress.$Geboortedatum.$Mail.$Groep)";
+?>
 <br>
 <img src="sneeuw.jpg" alt="sneeuw" width="230" height="240">
-
-
-
 </body>
 </html>
